5e8110f6bd073c033e34584e187516c41ebb3a73
muthomi254 <dmunene202020@gmail.com>
2023-09-18 22:58:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code in this file!
+
+// const currentUser = 'Grace Hopper';
+// const welcomeMessage ='Welcome to Flatbook, ';
+
+// const currentUser = 'Grace Hopper';
+// const welcomeMessage = currentUser;
+
+// const currentUser = 'Grace Hopper';
+// const welcomeMessage = 'Welcome to Flatbook, currentUser';
+
+// const currentUser = 'GraceHopper';
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+
+// const myString = 'template literal';
+// const myNumber = 10;
+// const myBoolean = false;
+// `Saying that interpolation with ${myString}s is better than concatenation ${90 + myNumber}% of the time is simply ${myBoolean} . But it is pretty cool!`
+
+const currentUser = 'Grace Hopper'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// const currentUser = 'Grace Hopper';
+  const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
